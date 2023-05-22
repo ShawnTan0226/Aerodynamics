1d234d873534f4b637e489076037ec0b2894568e
Paul Dumont <pdumont@student.tudelft.nl>
2023-05-22 13:21:51 +0200

    Working towards having a nice program that reads the airfoil dat file and returns max surface and total volume in the

--- Airfoil dat/BatterySize.py
+++ Airfoil dat/BatterySize.py
@@ -1,14 +1,13 @@
 ''' This code is made to compute the battery size in the wings'''
 
 import numpy as np
+import matplotlib.pyplot as plt
 '''Inputs'''
 
 A = 6 #Aspect ratio
-taper = 0.4 #Ratio tip over chord
-b = 40 #winspang [m]
-rho = 4 #Battery density [Wh/kg]
-epsilon = 4 #Batter density [Wh/m^3]
-E  = 40000 # Battery size [Wh]
+taper0 = 0.4 #Ratio tip over chord
+b0 = 40 # outer wing wingspan [m]
+V  = 40000 # Battery Volume [m^3]
 
 ''' Arifoil Properties '''
 S = 40
@@ -22,9 +21,11 @@ column2 = []
 # Read the .dat file
 file_path = "MH 91  14.98%.dat"  # Replace with the path to your .dat file
 
-# Initialize empty arrays
-column1 = []
-column2 = []
+# Initialize empty arrays for positive and negative values
+positive_column1 = []
+positive_column2 = []
+negative_column1 = []
+negative_column2 = []
 
 # Read the .dat file
 with open(file_path, 'r') as file:
@@ -35,11 +36,42 @@ with open(file_path, 'r') as file:
             value1 = float(data[0])
             value2 = float(data[1])
             if 0.15 <= value1 <= 0.55:
-                column1.append(value1)
-                column2.append(value2)
+                if value2 >= 0:
+                    positive_column1.append(value1)
+                    positive_column2.append(value2)
+                else:
+                    negative_column1.append(value1)
+                    negative_column2.append(value2)
 
-# Print the filtered arrays
-print("Filtered Column 1:", column1)
-print("Filtered Column 2:", column2)
+# Print the positive and negative arrays
+print("Positive Column 1:", positive_column1)
+print("Positive Column 2:", positive_column2)
+print("Negative Column 1:", negative_column1)
+print("Negative Column 2:", negative_column2)
+
+
+# Compute the surface using numpy
+postive_surface = -np.trapz(positive_column2, positive_column1)
+negative_surface = -np.trapz(negative_column2, negative_column1)
+
+# Plot the surface
+plt.plot(positive_column1, positive_column2, label='Positive')
+plt.plot(negative_column1, negative_column2, label='Negative')
+plt.xlabel('Column 1 (X-axis)')
+plt.ylabel('Column 2 (Y-axis)')
+plt.title('Surface Plot')
+plt.grid(True)
+ax = plt.gca()
+ax.set_aspect('equal', adjustable='box')
+plt.draw()
+plt.show()
+
+print(negative_surface)
+print(postive_surface)
+
+S= (negative_surface+postive_surface)
+print('Expected volume available for bateries : {} m^2 per chord of 1m'.format(S))
 
 '''Calculations'''
+
+
