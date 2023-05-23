6e215a277f4bda9723582bbc150ba396bedd0a53
Paul <dumontfeys.paul@gmail.com>
2023-05-23 11:51:50 +0200

    Added control

---
+++ Control.py
@@ -0,0 +1,13 @@
+
+
+
+
+
+#Mean aerodynamic chord
+def MAC(cr, ct, sweep, b):
+    #MAC
+    y = 2/3 * (cr + ct - (cr*ct)/(cr+ct))
+    #offset from that parts root
+    off_x = cr * 0.25 - ct * 0.25 + np.tan(sweep) * b / 2
+    off_y =
+    return y, off
