0fa64fe6adf058b3509b1bd17083c74e8a675aca
ShawnTan0226 <84079099+ShawnTan0226@users.noreply.github.com>
2023-05-26 10:37:46 +0200

    ard

--- BatterySize.py
+++ BatterySize.py
@@ -3,7 +3,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import math
-# from Plane import Plane
+from Plane import Plane
+from scipy.interpolate import interp1d
 '''Inputs'''
 
 g=9.81 #Gravitational acceleration [m/s^2]
@@ -143,7 +144,7 @@ Cri =  2 * S / ((taper_outer*x1+x1)*b_outer+(x1+1)*b_inner)
 
 plt.plot(b_inner,Cri, label='Inner wing')
 plt.plot(b_inner,x1, label='Inner wing')
-plt.show()
+# plt.show()
 
 print("b_outer: ",b_outer)
 print("b_inner: ",b_inner)
@@ -159,7 +160,7 @@ print("Offset outer: ",Cri*0.25-x1*Cri*taper_outer*0.25 + np.tan(sweep_outer)*b_
 
 
 class Planform_calculation:
-    def __init__(self,file_path_i,file_path_o,MTOW,Wing_loading,V_bat,V_body,b_frac,AR=6,sweep_inner=np.deg2rad(38),sweep_outer=np.deg2rad(38),taper_outer=0.267354977):
+    def __init__(self,file_path_i,file_path_o,MTOW,Wing_loading,V_bat,V_frac,b_frac,AR=6,sweep_inner=np.deg2rad(38),sweep_outer=np.deg2rad(38),taper_outer=0.267354977):
         self.g=9.81 #Gravitational acceleration [m/s^2]
         self.MTOW= MTOW #Maximum Take Off Weight [kg]
         self.Wing_loading= Wing_loading #Wing Loading [N/m^2]
@@ -167,7 +168,7 @@ class Planform_calculation:
         self.AR = AR #Aspect ratio
         b = np.sqrt(self.S*AR) # outer wing wingspan [m]
         self.V_bat  = V_bat # Battery Volume [m^3]
-        self.V_body = V_body # Battery Volume [m^3]
+        self.V_body = V_frac*V_bat # Battery Volume [m^3]
         self.V_tot = V_bat + V_body #Total Volume [m^3]
 
 
@@ -181,12 +182,20 @@ class Planform_calculation:
         self.Area_inner=self.airfoilvolume(file_path_i)
         self.Area_outer=self.airfoilvolume(file_path_o)
 
+
     def airfoilvolume(self,file_path):
         # Initialize empty arrays for positive and negative values
         positive_column1 = []
         positive_column2 = []
         negative_column1 = []
         negative_column2 = []
+
+        positive_column11 = []
+        positive_column21 = []
+        negative_column11 = []
+        negative_column21 = []
+        self.coords_negative=np.array([[0,0]]) # Initialize empty arrays for positive and negative values
+        self.coords_positive=np.array([[0,0]])
 
         # Read the .dat file
         with open(file_path, 'r') as file:
@@ -204,23 +213,42 @@ class Planform_calculation:
                             else:
                                 negative_column1.append(value1)
                                 negative_column2.append(value2)
+                            
+                    except ValueError:
+                        continue
+                    try:
+                        value1 = float(data[0])
+                        value2 = float(data[1])
+                        if value2 >= 0:
+                            positive_column11.append(value1)
+                            positive_column21.append(value2)
+                        else:
+                            negative_column11.append(value1)
+                            negative_column21.append(value2)
+                            
                     except ValueError:
                         continue
         # Compute the surface using numpy
         postive_surface = -np.trapz(positive_column2, positive_column1)
         negative_surface = -np.trapz(negative_column2, negative_column1)
         Area = (negative_surface+postive_surface)
+
+        eq_positive=interp1d(positive_column11[::-1],positive_column21[::-1],bounds_error=False,fill_value='extrapolate')
+        eq_negative=interp1d(negative_column11,negative_column21,bounds_error=False,fill_value='extrapolate')
+        thickness=eq_positive(np.arange(0,1,0.001))-eq_negative(np.arange(0,1,0.001))
+        self.max_thickness = np.max(thickness)
+        self.max_thickness_location = np.argmax(thickness)*0.001
         return Area
     def f(self,x): #In here x is the inner taper ratio
         Cri = 2 * self.S / ((self.taper_outer*x+x)*self.b_outer+(x+1)*self.b_inner)
         y=-self.V_tot + 2*self.Area_inner * (x**2*self.b_inner/2+0.5*(1-x)*x*self.b_inner+1/6*(1-x)**2*self.b_inner)*Cri**2+ 2*self.Area_outer * (self.taper_outer**2*self.b_outer/2+0.5*(1-self.taper_outer)*self.taper_outer*self.b_outer+1/6*(1-self.taper_outer)**2*self.b_outer)*x**2*Cri**2
         return y
 
-    def gradient(f, x, step):
+    def gradient(self,f, x, step):
         return (f(x + step) - f(x - step)) / (step * 2)
 
 
-    def newtonRaphson(f, x0, e, N, h, relax):
+    def newtonRaphson(self,f, x0, e, N, h, relax):
         print('\n\n*** NEWTON RAPHSON METHOD IMPLEMENTATION ***')
         i = 0
         step = 1
--- Main.py
+++ Main.py
@@ -1,4 +1,11 @@
 from Plane import Plane
+from BatterySize import Planform_calculation
+
+MakePlane = Planform_calculation(".\Airfoil_dat\MH 91  14.98%.dat",".\Airfoil_dat\MH 91  14.98%.dat",19902,2409,20.5,0.25,0.4)
+MakePlane.makeplane()
+MakePlane.plane.xflrvelues()
+MakePlane.plane.define_C_D_0(0.2)
+print(MakePlane.plane.C_D_0)
 
 test=Plane(9.72,[0.3,0.267,0.5],[38,38,5],[8.82,22,28])
 # test.plot_plane()
